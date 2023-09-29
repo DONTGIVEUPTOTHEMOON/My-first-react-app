513ba424faa27b37995014ffd401acd8c1fb4452
thananchanok <tonfern@thananchanoks-MacBook-Air.local>
2023-09-29 14:49:57 +0700

    go to git

---
+++ src/components/Greeting.tsx
@@ -0,0 +1,11 @@
+import classes from './Greeting.module.css'
+
+const Greeting = () => {
+  return (
+    <div className={classes.card}>
+      <h3>Wellcome!</h3>
+      <p>Tonfern</p>
+    </div>
+  )
+}
+export default Greeting
